3171fa504265bbde05410ae7ffb4ddc11e1c9e1a
Faizan-098 <faizan.imran607@gmail.com>
2026-01-22 00:18:14 +0500

    one chnage

--- store.js
+++ store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer from "../frontend/src/redux/features/authSlice.js"
-import userReducer from "../frontend/src/redux/features/userSlice.js"
-import postReducer from "../frontend/src/redux/features/postSlice.js"
-import profileReducer from "../frontend/src/redux/features/profile.js"
+import authReducer from "./src/redux/features/authSlice.js"
+import userReducer from "./src/redux/features/userSlice.js"
+import postReducer from "./src/redux/features/postSlice.js"
+import profileReducer from "./src/redux/features/profile.js"
  const store = configureStore({
   reducer: {
     auth:authReducer,
